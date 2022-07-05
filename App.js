e4924d3c00c854faa8111a974f33a3bf571ae7e3
Talbi Achref <achref.talbi@esprit.tn>
2022-07-05 13:49:46 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button,StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+<View>
+  <TextInput placeholder='Your course goal!' />
+  <Button title='add Goal' />
+</View>
+<View>
+  <Text>Liste of goals...</Text>
+</View>
+
+
+      
+    
+     
+  
+ 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer:{
+    padding:50
+  }
 });
